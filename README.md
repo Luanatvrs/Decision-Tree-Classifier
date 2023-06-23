d93d848fa433968f4034a66b835e61266b2ab2e1
Luanatvrs <luana.lorena@mail.uft.edu.br>
2023-06-22 22:11:53 -0300

    atualizando novo banco

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Decision-Tree-Classifier
 
-* Alunos: Luana Lorena de Matos Tavares, Pablo Henrique Silva Ribeiro, Arthur Fernandes Ferreira reis e Neudison Nonato Maia FIlho
+* Alunos: Luana Lorena de Matos Tavares, Pablo Henrique Silva Ribeiro, Arthur Fernandes Ferreira reis, Neudison Nonato Maia FIlho e Plácido de Aquino Angelim Neto
 * Disciplina: Inteligência Artificial
 * Professora: Glenda Michele Botelho
--- decisionTreeDataset.py
+++ decisionTreeDataset.py
@@ -3,14 +3,14 @@ import pandas as pd
 from sklearn.discriminant_analysis import StandardScaler
 from sklearn.inspection import DecisionBoundaryDisplay
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import  classification_report, accuracy_score, confusion_matrix
+from sklearn.metrics import  ConfusionMatrixDisplay, classification_report, accuracy_score, confusion_matrix
 from sklearn.preprocessing import LabelEncoder
 import numpy as np
 from sklearn.tree import DecisionTreeClassifier
 import time
 
 inicio = time.time()
-data = pd.read_excel('D:/IA/Decision-Tree-Classifier/dataset.xlsx')
+data = pd.read_csv('D:/IA/Decision-Tree-Classifier/Titanic-Dataset.csv')
 
 data = data.dropna(axis=1, how='all')
 
@@ -20,13 +20,14 @@ for y in data.columns:
         lbl.fit(list(data[y].values))
         data[y] = lbl.transform(list(data[y].values))
 
-colunas = ['Patient ID', 'Patient age quantile', 'SARS-Cov-2 exam result',
-           'Patient addmited to regular ward (1=yes, 0=no)',
-           'Patient addmited to semi-intensive unit (1=yes, 0=no)',
-           'Patient addmited to intensive care unit (1=yes, 0=no)']
+colunas = ['PassengerId',  'Survived', 'Name', 'Ticket', 'Fare',
+           'Cabin',
+           'Embarked',
+           ]
 
 dt_copy = data.copy()
 dt_copy.drop(colunas, axis=1, inplace=True)
+print(dt_copy)
 
 for column in dt_copy.columns:
 
@@ -41,13 +42,13 @@ for column in dt_copy.columns:
 
     dt_copy.loc[outliers, column] = np.nan
 
-dt_copy = dt_copy.join(data['SARS-Cov-2 exam result'])
+dt_copy = dt_copy.join(data['Survived'])
 
 dt_copy = dt_copy[dt_copy.columns[dt_copy.isna().sum()/dt_copy.shape[0] < 0.9]]
 dt_copy = dt_copy.fillna(dt_copy.median())
 
 # exibindo correlações
-correlations = dt_copy.corrwith(dt_copy['SARS-Cov-2 exam result'])
+correlations = dt_copy.corrwith(dt_copy['Survived'])
 print(correlations.sort_values(ascending=False).head(10))
 print('\n\n')
 print(correlations.sort_values(ascending=True).head(10))
@@ -56,22 +57,20 @@ print(correlations.abs().sort_values(ascending=False).head(10))
 print('\n\n')
 
 # criando listas X (features) e y (resultado esperado)
-X = dt_copy.drop(columns=['SARS-Cov-2 exam result'])
+X = dt_copy.drop(columns=['Survived'])
 
-X = dt_copy[['Mean platelet volume ', 'Platelets']]
+X = dt_copy[['Pclass', 'Sex', 'Age', 'SibSp', 'Parch']]
 
 scaler =StandardScaler().fit(X)
 X = scaler.transform(X)
 
-y = dt_copy['SARS-Cov-2 exam result']
+y = dt_copy['Survived']
 
 # separando 66% do dataset para treinamento e 33% para validação
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.33, random_state=101)
 
 names = [
-    "Nearest Neighbors",
     "Decision Tree",
-    "Naive Bayes",
 ]
 
 classifiers = [
@@ -94,20 +93,11 @@ for model_names, model in zip(names,classifiers):
     print(f'Model {model_names}:\n\n'
           f'tn {tn}, fp {fp}, fn {fn}, tp {tp}\n\n'
           f'Accuracy: {acs}\n\n'
-      f'Classification Report:\n{classification_report(y_test, prds)}\n')
+      f'Classification Report:\n{classification_report(y_test, prds, target_names = ["morreu", "sobreviveu"])}\n')
     
-    disp = DecisionBoundaryDisplay.from_estimator(
-    model, X_train, response_method="predict",
-    xlabel="Mean platelet volume", ylabel="Platelets",
-    alpha=0.5,
-    
-    )
     fim = time.time()
     tempo_de_processamento = fim - inicio
     print(f"O tempo de processamento foi de {tempo_de_processamento} segundos.")
-
-    disp.ax_.scatter(X_train[:, 0], X_train[:, 1], c=y_train, edgecolor="k")
-    plt.title(f'{model_names}')
-    plt.axis('tight')
-
-plt.show()
+    
+    ConfusionMatrixDisplay.from_estimator(model, X_test, y_test, display_labels=['morreu', 'sobreviveu'], cmap='Blues', values_format='d')
+    plt.show()
